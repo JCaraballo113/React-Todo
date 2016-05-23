cd6371dcc5cec677343fbf7a3527531050d0a89d
John Caraballo <jcaraballo019@gmail.com>
2016-05-23 17:59:27 -0400

    Added TodoList functionality

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,11 +1,36 @@
 var React = require('react');
 
+var TodoList = require('TodoList');
+
 //A stateless component
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the yard'
+        },
+        {
+          id: 3,
+          text: 'Pet Hershey'
+        },
+        {
+          id: 4,
+          text: 'Pet Midnight'
+        }
+      ],
+    };
+  },
   render: function() {
+    var {todos} = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     )
   }
